6dff997e544a3e01845539b8ab1fd86204607eaa
ashanhq <20064191@mydbs.ie>
2025-09-30 02:56:44 +0530

    Create ex1.js

---
+++ ex1.js
@@ -0,0 +1,15 @@
+function check() {
+    const weightInput = document.getElementById('weight').value;
+    const weight = parseFloat(weightInput);
+
+    if (isNaN(weight)) {
+        alert("Please enter a valid number for baggage weight.");
+        return;
+    }
+
+    if (weight > 15) {
+        alert("Overweight baggage! (" + weight + " kg)");
+    } else {
+        alert("Baggage weight is OK. (" + weight + " kg)");
+    }
+}
